c9875f0c760ae5b4685c78a78368524f4a4eac5b
ntnyq <ntnyq13@gmail.com>
2025-08-20 17:40:44 +0800

    ci: setup trusted publisher

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,10 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     environment: 'happy-dom',
-    reporters: ['dot'],
-    coverage: {
-      include: ['./src/**/*.ts'],
-      reporter: ['lcov', 'text'],
-    },
+    watch: false,
   },
 })
