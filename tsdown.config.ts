02b0259deb82f82138646dae6c50ff916d6eebfa
ntnyq <ntnyq13@gmail.com>
2025-11-20 13:22:46 +0800

    build: set platform to browser

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
   clean: true,
   dts: true,
   entry: ['src/index.ts'],
+  platform: 'browser',
   hooks: {
     'build:done': async () => {
       await x('npm', ['run', 'build:style'])
